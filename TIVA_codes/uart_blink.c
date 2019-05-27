919034dfd4c0db976f47871f71a7814f03fa5ff5
Mayuresh9 <mayureshbhoyar9@gmail.com>
2019-05-27 17:33:16 +0530

    send what is recieved using UART and blink when recieved H character

---
+++ TIVA_codes/uart_blink.c
@@ -0,0 +1,52 @@
+/*
+ * ua.c
+ *
+ *  Created on: 27-May-2019
+ *      Author: Mayuresh
+ */
+#include "stdint.h"
+#include "stdbool.h"
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_sysctl.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/uart.h"
+#include "driverlib/pin_map.h"
+
+int main()
+{
+    //SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE,GPIO_PIN_0 | GPIO_PIN_1);
+    UARTEnable(UART0_BASE);
+    UARTConfigSetExpClk(UART0_BASE,SysCtlClockGet(), 9600,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE,GPIO_PIN_1);
+    int32_t t;
+    UARTCharPut(UART0_BASE,'H');
+    while(1)
+    {
+        if(UARTCharsAvail((UART0_BASE)))
+        {
+            t=UARTCharGetNonBlocking(UART0_BASE);
+                    UARTCharPutNonBlocking(UART0_BASE,t);
+                if(t=='H')
+                {
+                    UARTCharPut(UART0_BASE,t);
+                    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1,0xFF);
+                    SysCtlDelay(1000);
+                    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1,0x00);
+                    SysCtlDelay(1000);
+                }
+        }
+
+    }
+
+
+}
